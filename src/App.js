82a997f4c6243bbc1a6c86e315f534b38b6372e1
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-26 19:24:34 -0300

    added Parent&ChildComponent,& ParentComponentTest, testing ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter  from './Counter';
+import ParentComponent from './ParentComponent';
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <ParentComponent />
     </div>
   );
 }
